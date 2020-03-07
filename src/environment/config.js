5ee8c7fe08ea788b6c1234c1593acc0e708e0371
Marcus Goldschmidt Oliveira <marcus.golds@hotmail.com>
2020-03-07 18:00:42 -0400

    Adicionado dotenv

---
+++ src/environment/config.js
@@ -0,0 +1,11 @@
+export default {
+  BASE_URL: process.env.BASE_URL,
+
+  AUTH: {
+    CLIENT_ID: process.env.CLIENT_ID,
+    CLIENT_SECRET: process.env.CLIENT_SECRET,
+    AUTH_AUTHORIZATION_ENDPOINT: process.env.AUTH_AUTHORIZATION_ENDPOINT,
+    AUTH_REDIRECT_URI: process.env.AUTH_REDIRECT_URI,
+    AUTH_URI: process.env.AUTH_URI,
+  }
+}
