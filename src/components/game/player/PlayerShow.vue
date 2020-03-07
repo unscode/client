02e116d66cd202d896954f93ef1ac14412bbd2f5
Jorge Lauro Campos Rodrigues <jorgerodrigues9@outlook.com>
2020-03-07 14:13:47 -0400

    Atualizado funcionalidade para vincular pontos do jogo para o jogador

--- src/components/game/player/PlayerShow.vue
+++ src/components/game/player/PlayerShow.vue
@@ -1,10 +1,14 @@
 <template>
   <div>
-    <h1 class="fade-in" v-if="player.name">{{player.name}}</h1>
+    <h1 class="fade-in"
+        v-if="player.name"
+    >
+      Jogador {{player.name}}
+    </h1>
     <pacman v-show="waiting"/>
     <!-- Medals -->
-    <div>
-      <h3>Medalha(s) do jogador</h3>
+    <div class="drop">
+      <h3>Medalha(s)</h3>
       <!-- Player Medals -->
       <div class="medals">
         <draggable v-model="player.medals"
@@ -27,7 +31,7 @@
       </div>
       <!-- Trash Medals -->
       <div class="remove"
-           v-if="player.medals ? player.medals.length : false"
+           v-if="player.medals && player.medals.length"
       >
         <div class="trash"
              @add="trashMedal"
@@ -41,10 +45,15 @@
           </draggable>
         </div>
       </div>
+      <p class="empty"
+         v-if="player.medals && !player.medals.length"
+      >
+        Sem medalhas, arraste e solte aqui para adicionar
+      </p>
     </div>
     <!-- Score -->
-    <div>
-      <h3>Ponto(s) do jogador</h3>
+    <div class="drop">
+      <h3>Ponto(s)</h3>
       <!-- Player Scores -->
       <div class="scores">
         <draggable v-model="player.scores"
@@ -67,7 +76,7 @@
       </div>
       <!-- Trash Scores -->
       <div class="remove"
-           v-if="player.scores ? player.scores.length: false"
+           v-if="player.scores && player.scores.length"
       >
         <div class="trash"
              @add="trashScore"
@@ -81,6 +90,11 @@
           </draggable>
         </div>
       </div>
+      <p class="empty"
+         v-if="player.scores && !player.scores.length"
+      >
+        Sem pontos, arraste e solte aqui para adicionar
+      </p>
     </div>
   </div>
 </template>
@@ -142,8 +156,34 @@
       },
       detroyPlayerMedal(medalId) {
         this.waiting = true;
-        const data = { id: medalId };
         this.$auth.$http.delete(`game/${this.gameId}/player/${this.playerId}/medal/${medalId}`)
+          .then(response => {
+            this.requestPlayer();
+          })
+          .catch(error => {
+
+          })
+          .finally(() => {
+            this.waiting = false;
+          });
+      },
+      storePlayerScore(scoreId) {
+        this.waiting = true;
+        const data = { id: scoreId };
+        this.$auth.$http.post(`game/${this.gameId}/player/${this.playerId}/score`, data)
+          .then(response => {
+            this.requestPlayer();
+          })
+          .catch(error => {
+
+          })
+          .finally(() => {
+            this.waiting = false;
+          });
+      },
+      destroyPlayerScore(scoreId) {
+        this.waiting = true;
+        this.$auth.$http.delete(`game/${this.gameId}/player/${this.playerId}/score/${scoreId}`)
           .then(response => {
             this.requestPlayer();
           })
@@ -161,9 +201,10 @@
         this.detroyPlayerMedal(this.player.medals[e.newIndex].id);
       },
       attachScore(e) {
+        this.storePlayerScore(this.player.scores[e.newIndex].id);
       },
       trashScore(e) {
-
+        this.destroyPlayerScore(this.player.scores[e.newIndex].id);
       }
     },
     mounted() {
@@ -191,6 +232,31 @@
     background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1600 900'%3E%3Cdefs%3E%3ClinearGradient id='a' x1='0' x2='0' y1='1' y2='0' gradientTransform='rotate(96,0.5,0.5)'%3E%3Cstop offset='0' stop-color='%230FF'/%3E%3Cstop offset='1' stop-color='%23CF6'/%3E%3C/linearGradient%3E%3ClinearGradient id='b' x1='0' x2='0' y1='0' y2='1' gradientTransform='rotate(84,0.5,0.5)'%3E%3Cstop offset='0' stop-color='%23F00'/%3E%3Cstop offset='1' stop-color='%23FC0'/%3E%3C/linearGradient%3E%3C/defs%3E%3Cg fill='%23FFF' fill-opacity='0' stroke-miterlimit='10'%3E%3Cg stroke='url(%23a)' stroke-width='7.919999999999999'%3E%3Cpath transform='translate(-103.25 6.800000000000001) rotate(-0.25 1409 581) scale(0.95277)' d='M1409 581 1450.35 511 1490 581z'/%3E%3Ccircle stroke-width='2.64' transform='translate(-122 77) rotate(8.1 800 450) scale(1.00864)' cx='500' cy='100' r='40'/%3E%3Cpath transform='translate(21.299999999999997 -96) rotate(66 401 736) scale(1.00864)' d='M400.86 735.5h-83.73c0-23.12 18.74-41.87 41.87-41.87S400.86 712.38 400.86 735.5z'/%3E%3C/g%3E%3Cg stroke='url(%23b)' stroke-width='2.4'%3E%3Cpath transform='translate(462 -0.8000000000000007) rotate(0.20000000000000018 150 345) scale(0.98159)' d='M149.8 345.2 118.4 389.8 149.8 434.4 181.2 389.8z'/%3E%3Crect stroke-width='5.28' transform='translate(-143 -207.5) rotate(61.19999999999999 1089 759)' x='1039' y='709' width='100' height='100'/%3E%3Cpath transform='translate(-294 82) rotate(10.2 1400 132) scale(0.85)' d='M1426.8 132.4 1405.7 168.8 1363.7 168.8 1342.7 132.4 1363.7 96 1405.7 96z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
     background-attachment: fixed;
     cursor: pointer;
+    transition: all 200ms ease-in-out;
+  }
+
+  .drop {
+    margin-bottom: 10px;
+    padding: 15px;
+    border: 2px solid black;
+    border-radius: $border-radius;
+  }
+
+  .medals,
+  .scores {
+
+    .medal,
+    .score {
+      margin: 1%;
+      width: 92%;
+      display: inline-block;
+      @media (min-width: map_get($grid-breakpoints, 'md')) {
+        width: 56%;
+      }
+      @media (min-width: map_get($grid-breakpoints, 'lg')) {
+        width: 23%;
+      }
+    }
   }
 
   .remove {
@@ -217,24 +283,8 @@
     }
   }
 
-  .medals,
-  .scores {
-    margin: {
-      left: -1%;
-      right: -1%;
-    };
-
-    .medal,
-    .score {
-      margin: 1%;
-      width: 92%;
-      display: inline-block;
-      @media (min-width: map_get($grid-breakpoints, 'md')) {
-        width: 56%;
-      }
-      @media (min-width: map_get($grid-breakpoints, 'lg')) {
-        width: 23%;
-      }
-    }
+  .empty {
+    padding: 5px;
+    text-align: center;
   }
 </style>
