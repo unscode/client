dc0b98125dbe27aa6cb0f1912f61f076f41248dd
Jorge Lauro Campos Rodrigues <jorgerodrigues9@outlook.com>
2020-03-07 16:58:37 -0400

    Alterado sttilo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,11 @@
 <template>
-  <div class="home">
-    <Navbar/>
-    <div class="container">
-      <HelloWorld msg="Welcome"/>
-    </div>
-  </div>
+  <div></div>
 </template>
 
 <script>
-  // @ is an alias to /src
-  import Navbar from '../components/Navbar';
-  import HelloWorld from '@/components/HelloWorld.vue';
 
-  export default {
-    name: 'Home',
-    components: {
-      Navbar,
-      HelloWorld,
-    },
-  };
 </script>
 
 <style lang="scss" scoped>
-  @import "~bootstrap/scss/functions";
-  @import "../assets/styles/variables";
-  @import "~bootstrap/scss/variables";
-  @import "~bootstrap/scss/mixins";
-  @import "~bootstrap/scss/buttons";
-  @import "~bootstrap/scss/grid";
+
 </style>
